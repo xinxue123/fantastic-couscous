7e0d37ce17db4716e63998607fe073aad070ae79
Lihao <lihao-cugb@outlook.com>
2025-03-24 11:01:40 +0800

    auto ad

--- source/conf.py
+++ source/conf.py
@@ -58,3 +58,6 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_js_files = [
+    "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9666808780669786"
+]
